c5dd40ba0e90624c35f91595de219ff1dd6ebe98
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-01-19 14:45:25 +0000

    Adds score image to homepage

---
+++ index.js
@@ -0,0 +1,30 @@
+// Header
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+    connectedCallback() {
+      this.innerHTML = `
+      <header role="banner">
+        <div id="logo">
+            <h1 role="heading" alt="home"><a href="/">Ming Yang</a></h1>
+        </div>
+        <div id="nav">
+            <nav role="navigation">
+                <a href="#">about</a>
+                <div class="dropdown">
+                    <button class="dropbutton" role="button">works ⌄</button>
+                    <div class="dropdown-content">
+                        <a href="#" role="link">concert pieces</a>
+                        <a href="#" role="link">installations</a>
+                        <a href="#" role="link">experiments</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+      </header>
+    `;
+    }
+  }
+  
+  customElements.define("header-component", Header);
